d18298a503ced32168593e5524f56a6f0e106c4f
zjwu46 <zjwu46@gmail.com>
2023-09-19 20:03:43 -0400

    test robot

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -109,9 +109,10 @@ public class Swerve extends SubsystemBase {
   }
 
   public Rotation2d getYaw() {
-    return (Constants.Swerve.invertGyro)
-        ? Rotation2d.fromDegrees(360 - gyro.getYaw())
-        : Rotation2d.fromDegrees(gyro.getYaw());
+    //return (Constants.Swerve.invertGyro)
+    //    ? Rotation2d.fromDegrees(360 - gyro.getYaw())
+    //    : Rotation2d.fromDegrees(gyro.getYaw());
+    return Rotation2d.fromDegrees(gyro.getYaw());
   }
 
 
